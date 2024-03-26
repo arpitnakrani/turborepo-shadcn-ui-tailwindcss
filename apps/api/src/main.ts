a38fd4b0c3c40df55529ba811a75efa7f77d9875
arpit nakrani <arpitnakrani2580@gmail.com>
2024-03-26 14:29:00 +0530

    middleware in nest js with fastify

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,13 +4,17 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { LoggerMiddleware } from './common/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
-  await app.listen(3000);
-  console.log("Server started on the port 3000")
+
+   // Apply your custom middleware
+   app.use(new LoggerMiddleware().use);
+   await app.listen(3000);
+  console.log("api Server started on the port 3000")
 }
 bootstrap();
