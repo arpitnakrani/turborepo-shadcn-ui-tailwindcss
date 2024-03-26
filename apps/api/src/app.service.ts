179d1f40e79ae122ed830e81dd1d5e1624080e3a
arpit nakrani <arpitnakrani2580@gmail.com>
2024-03-26 14:47:56 +0530

    logging using pino

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { error } from 'console';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
+
   getHello(): string {
+    // this.logger.log(`Retrieve all AppService`);
+    this.logger.error({ id: `retrieve-all-AppService-error` }, `Retrieve all AppService`) // object passed in first argument
+    throw new Error("fdfsdfdf")
+
     return 'Hello World!';
   }
 }
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,16 +5,22 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { LoggerMiddleware } from './common/logger.middleware';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter()
-  );
+      const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter()
+      );
 
-   // Apply your custom middleware
-   app.use(new LoggerMiddleware().use);
-   await app.listen(3000);
-  console.log("api Server started on the port 3000")
+      // Apply your custom middleware
+      //  app.use(new LoggerMiddleware().use);
+
+      //Apply pino logger
+      app.useLogger(app.get(Logger));
+      app.useGlobalInterceptors(new LoggerErrorInterceptor());
+
+      await app.listen(3000);
+      console.log("api Server started on the port 3000")
 }
 bootstrap();
