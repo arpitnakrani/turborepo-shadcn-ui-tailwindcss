fb57ea95b6f7edbeb20fd5a296f81488fe6857ba
arpit nakrani <arpitnakrani2580@gmail.com>
2024-05-03 14:59:04 +0530

    remove auth microservice  and added as github submodule

--- apps/auth/src/supabase/supabase.service.ts
+++
@@ -1,9 +0,0 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { SupabaseClient, createClient } from '@supabase/supabase-js'
-import { SupabaseAsyncProvider } from './supabase.provider';
-
-@Injectable()
-export class SupabaseService {
-    constructor(@Inject(SupabaseAsyncProvider) readonly supabase: SupabaseClient) {}
-
-}
